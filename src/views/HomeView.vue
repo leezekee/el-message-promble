76caf93e2deecfb7cba134744d181a0501792398
zekee <841361034@qq.com>
2023-05-21 01:30:59 +0800

    commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-button type="primary" @click="clickHandler">Primary</el-button>
+</template>
+
+<script setup>
+import { ElMessage } from 'element-plus';
+const clickHandler = () => {
+  ElMessage.closeAll()
+  ElMessage({
+    message: '这是第一条提醒',
+    type: 'success',
+    duration: 3 * 1000
+  })
+  // ElMessage.closeAll()
+  // ElMessage({
+  //   message: '这是第二条提醒',
+  //   type: 'success',
+  //   duration: 3 * 1000
+  // })
+  setTimeout(() => {
+    ElMessage.closeAll()
+    ElMessage({
+      message: '这是第二条提醒',
+      type: 'success',
+      duration: 3 * 1000
+    })
+  }, 3000);
+}
+</script>
